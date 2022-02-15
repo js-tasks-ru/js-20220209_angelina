c074992bff501bb70dbbcf2b1e6e74e825eaabb1
angelinagorbachevskaya <angelinagorbachevskaya@cloud.upwork.com>
2022-02-15 11:08:18 +0300

    resolve sort-strings (lesson2, task1)

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,35 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    let buffer = '';
+    let sortArr = [...arr];
+    let isSorted = false;
+    let collator = new Intl.Collator('ru-RU', {caseFirst: "upper"});
+    if (param === 'asc') {
+        while (!isSorted) {
+            isSorted = true;
+            for (let i = 0; i < sortArr.length - 1; i++) {
+                if (collator.compare(sortArr[i], sortArr[i+1]) === 1) {
+                    isSorted = false;
+                    buffer = sortArr[i];
+                    sortArr[i] = sortArr[i+1];
+                    sortArr[i+1] = buffer;
+                }
+            }
+        }
+        return sortArr; 
+    } else if (param === 'desc') {
+        while (!isSorted) {
+            isSorted = true;
+            for (let i = 0; i < sortArr.length - 1; i++) {
+                if (collator.compare(sortArr[i], sortArr[i+1]) === -1) {
+                    isSorted = false;
+                    buffer = sortArr[i+1];
+                    sortArr[i+1] = sortArr[i];
+                    sortArr[i] = buffer;
+                }
+            }
+        } 
+        return sortArr;
+    }
 }
