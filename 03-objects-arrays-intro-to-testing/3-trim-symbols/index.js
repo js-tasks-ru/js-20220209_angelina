8336577e296810ed8f4d550a5ec9800688bbe69d
angelinagorbachevskaya <angelinagorbachevskaya@cloud.upwork.com>
2022-02-19 18:56:25 +0300

    resolve 3 task

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    let start = 0;
+    let result = '';
 
+    while (start < string.length) {
+        let c = string[start];
+        let q = 0;
+        while (string.indexOf(c, start + q) === start + q) {
+            q++;
+        }
+        console.log("q: " + q);
+        if (q > size) {
+            result += string.substring(start, start + size);
+            console.log("IF.result: " + result);
+        } else {
+            result += string.substring(start, start + q);
+            console.log("ELSE.result: " + result);
+        }
+        start += q;
+    }
+    return result;
 }
