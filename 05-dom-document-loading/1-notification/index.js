35f0d4ad82dea0875d92acc87a15764285a1add8
angelinagorbachevskaya <angelinagorbachevskaya@cloud.upwork.com>
2022-02-26 19:18:22 +0300

    part of notification task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
+    // static duration;
+    static counter = 0;;
 
+    constructor(message = '',
+        {
+            duration = 0,
+            type = ''
+        } = {}) {
+            this.message = message;
+            this.duration = duration;
+            this.type = type;
+            NotificationMessage.counter++;
+
+            this.render();
+    }
+
+    getTemplate() {
+        return `
+        <div class="notification" style="--value:20s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    render() {
+        const element = document.createElement('div');
+        
+        element.innerHTML = this.getTemplate();
+
+        this.element = element.firstElementChild;
+
+        if (this.type !== '') {
+            this.element.classList.add(this.type);
+        }
+    }
+
+    show(parentElement = {}) {
+        if (NotificationMessage.counter < 2) {
+            parentElement ? parentElement.append(this.element) : document.body.append(this.element);
+        }
+        // update timer
+        this.element.style["--value"] = '20s';
+        console.log(this.element.style);
+    }
+
+    destroy() {
+        this.element.remove();
+    }
 }
